6496821f9d4a3c36bd08cada3e33c0afad0bb6b1
Andrey Smirnov <andrew.smirnov@gmail.com>
2018-02-04 10:22:05 -0800

    pci: Use pci_config_size in pci_data_* accessors
    Use pci_config_size (as opposed to PCI_CONFIG_SPACE_SIZE) in
    pci_data_read() and pci_data_write(), so this function would work for
    both classic PCI and PCIe use-cases.
    
    Cc: Peter Maydell <peter.maydell@linaro.org>
    Cc: Jason Wang <jasowang@redhat.com>
    Cc: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Cc: Marcel Apfelbaum <marcel.apfelbaum@zoho.com>
    Cc: Michael S. Tsirkin <mst@redhat.com>
    Cc: qemu-devel@nongnu.org
    Cc: qemu-arm@nongnu.org
    Cc: yurovsky@gmail.com
    Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>

--- hw/pci/pci_host.c
+++ hw/pci/pci_host.c
@@ -89,30 +89,35 @@ uint32_t pci_host_config_read_common(PCIDevice *pci_dev, uint32_t addr,
 void pci_data_write(PCIBus *s, uint32_t addr, uint32_t val, int len)
 {
     PCIDevice *pci_dev = pci_dev_find_by_addr(s, addr);
-    uint32_t config_addr = addr & (PCI_CONFIG_SPACE_SIZE - 1);
+    uint32_t config_addr;
 
     if (!pci_dev) {
         return;
     }
 
+    config_addr = addr & (pci_config_size(pci_dev) - 1);
+
     PCI_DPRINTF("%s: %s: addr=%02" PRIx32 " val=%08" PRIx32 " len=%d\n",
                 __func__, pci_dev->name, config_addr, val, len);
-    pci_host_config_write_common(pci_dev, config_addr, PCI_CONFIG_SPACE_SIZE,
+    pci_host_config_write_common(pci_dev, config_addr,
+                                 pci_config_size(pci_dev),
                                  val, len);
 }
 
 uint32_t pci_data_read(PCIBus *s, uint32_t addr, int len)
 {
     PCIDevice *pci_dev = pci_dev_find_by_addr(s, addr);
-    uint32_t config_addr = addr & (PCI_CONFIG_SPACE_SIZE - 1);
+    uint32_t config_addr;
     uint32_t val;
 
     if (!pci_dev) {
         return ~0x0;
     }
 
+    config_addr = addr & (pci_config_size(pci_dev) - 1);
+
     val = pci_host_config_read_common(pci_dev, config_addr,
-                                      PCI_CONFIG_SPACE_SIZE, len);
+                                      pci_config_size(pci_dev), len);
     PCI_DPRINTF("%s: %s: addr=%02"PRIx32" val=%08"PRIx32" len=%d\n",
                 __func__, pci_dev->name, config_addr, val, len);
 
