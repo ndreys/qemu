1b41b57f131357477934d066e4e0be2d8da638af
Andrey Smirnov <andrew.smirnov@gmail.com>
2018-03-05 08:51:15 -0800

    hw/misc: imx7_snvs: Add support for non-volatile LPGPR registers
    TODO: Add code for graceful degradation when no BlockBackend is
    provided.
    
    Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>

--- hw/misc/imx7_snvs.c
+++ hw/misc/imx7_snvs.c
@@ -13,23 +13,58 @@
  */
 
 #include "qemu/osdep.h"
+#include "qapi/error.h"
 #include "hw/misc/imx7_snvs.h"
 #include "qemu/log.h"
 #include "sysemu/sysemu.h"
+#include "sysemu/block-backend.h"
+#include "sysemu/blockdev.h"
 
 static uint64_t imx7_snvs_read(void *opaque, hwaddr offset, unsigned size)
 {
+    IMX7SNVSState *s = IMX7_SNVS(opaque);
+    uint32_t value = 0xdeadbeef;
+
+    switch (offset) {
+    case SNVS_LPGPR0: /* FALLTHROUGH */
+    case SNVS_LPGPR1: /* FALLTHROUGH */
+    case SNVS_LPGPR2: /* FALLTHROUGH */
+    case SNVS_LPGPR3:
+        if (s->lpgpr) {
+            blk_pread(s->lpgpr, offset - SNVS_LPGPR0, &value, sizeof(value));
+        }
+        return value;
+    }
+
     return 0;
 }
 
 static void imx7_snvs_write(void *opaque, hwaddr offset,
                             uint64_t v, unsigned size)
 {
+    IMX7SNVSState *s = IMX7_SNVS(opaque);
     const uint32_t value = v;
-    const uint32_t mask  = SNVS_LPCR_TOP | SNVS_LPCR_DP_EN;
 
-    if (offset == SNVS_LPCR && ((value & mask) == mask)) {
-        qemu_system_shutdown_request(SHUTDOWN_CAUSE_GUEST_SHUTDOWN);
+    switch (offset) {
+    case SNVS_LPCR: {
+        const uint32_t mask  = SNVS_LPCR_TOP | SNVS_LPCR_DP_EN;
+
+        if ((value & mask) == mask) {
+            qemu_system_shutdown_request(SHUTDOWN_CAUSE_GUEST_SHUTDOWN);
+        }
+        break;
+    }
+    case SNVS_LPGPR0: /* FALLTHROUGH */
+    case SNVS_LPGPR1: /* FALLTHROUGH */
+    case SNVS_LPGPR2: /* FALLTHROUGH */
+    case SNVS_LPGPR3:
+        if (s->lpgpr) {
+            blk_pwrite(s->lpgpr, offset - SNVS_LPGPR0, &value,
+                       sizeof(value), 0);
+        }
+        break;
+    default:
+        break;
     }
 }
 
@@ -59,6 +94,24 @@ static void imx7_snvs_init(Object *obj)
                           TYPE_IMX7_SNVS, 0x1000);
 
     sysbus_init_mmio(sd, &s->mmio);
+
+    s->lpgpr = blk_by_name("snvs-lpgpr");
+    if (s->lpgpr) {
+        if (blk_getlength(s->lpgpr) < SNVS_LPGPR_NUM * sizeof(uint32_t)) {
+            qemu_log_mask(LOG_GUEST_ERROR,
+                          "%s: BlockBackend is too small. Ignoring it.\n",
+                          __func__);
+            s->lpgpr = NULL;
+            return;
+        }
+
+        blk_set_perm(s->lpgpr, BLK_PERM_CONSISTENT_READ | BLK_PERM_WRITE,
+                     BLK_PERM_ALL, &error_fatal);
+    } else {
+        qemu_log_mask(LOG_GUEST_ERROR,
+                      "%s: No BlockBackend provided to store LPGPR state\n",
+                      __func__);
+    }
 }
 
 static void imx7_snvs_class_init(ObjectClass *klass, void *data)
--- include/hw/misc/imx7_snvs.h
+++ include/hw/misc/imx7_snvs.h
@@ -19,7 +19,13 @@
 enum IMX7SNVSRegisters {
     SNVS_LPCR = 0x38,
     SNVS_LPCR_TOP   = BIT(6),
-    SNVS_LPCR_DP_EN = BIT(5)
+    SNVS_LPCR_DP_EN = BIT(5),
+
+    SNVS_LPGPR0 = 0x90,
+    SNVS_LPGPR1 = 0x94,
+    SNVS_LPGPR2 = 0x98,
+    SNVS_LPGPR3 = 0x9c,
+    SNVS_LPGPR_NUM = 4,
 };
 
 #define TYPE_IMX7_SNVS "imx7.snvs"
@@ -30,6 +36,7 @@ typedef struct IMX7SNVSState {
     SysBusDevice parent_obj;
 
     MemoryRegion mmio;
+    BlockBackend *lpgpr;
 } IMX7SNVSState;
 
 #endif /* IMX7_SNVS_H */
